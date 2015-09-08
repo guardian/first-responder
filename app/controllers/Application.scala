cc600acf19a7a2aa4d9ef27648f272b0b54ac0b1
Chris Wilk <chris.wilk@theguardian.com>
2015-09-08 16:27:48 +0100

    More styling work - hurray

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,6 +3,7 @@ package controllers
 import com.gu.googleauth.GoogleAuthConfig
 import models._
 import play.api.mvc._
+import org.joda.time.DateTime
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -10,7 +11,16 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class Application(val authConfig: GoogleAuthConfig) extends Controller with AuthActions {
 
   def index = AuthAction { request =>
-    Ok(views.html.index(request.user.firstName))
+
+    // Stub callouts and contributions
+    val callouts = Seq(new Callout("refugeecrisis", DateTime.now, ""), new Callout("bangkokbombing", DateTime.now, ""))
+
+    val contributions = Seq(
+      new Contribution("1", new Contributor(Some("chris.wilk@guardian.co.uk")), "refugeecrisis", "Tester", Nil),
+      new Contribution("2", new Contributor(Some("chris.wilk@guardian.co.uk")), "refugeecrisis", "Tester 2", Nil)
+    )
+
+    Ok(views.html.index(callouts, contributions))
   }
 
   def healthcheck = Action {
