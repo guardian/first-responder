4635f58c148570ba8f93eed8f37258c712a36ea0
Chris Birchall <chris.birchall@guardian.co.uk>
2015-09-11 11:45:39 +0100

    Add Formstack embed to widget

---
+++ app/formstack/FormstackEmbedder.scala
@@ -0,0 +1,21 @@
+package formstack
+
+import play.api.libs.json._
+import play.api.libs.ws.WSAPI
+import play.twirl.api.Html
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class FormstackEmbedder(ws: WSAPI, formstackOAuthToken: String) {
+
+  def getEmbedCode(formId: String): Future[Html] = {
+    ws.url(s"https://www.formstack.com/api/v2/form/$formId.json?oauth_token=$formstackOAuthToken")
+      .withHeaders("Content-Type" -> "application/json")
+      .get()
+      .map { response =>
+        Html((response.json \ "javascript").as[String])
+      }
+  }
+
+}
