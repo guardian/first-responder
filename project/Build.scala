6d8dc3c6545aa697faf30b4e413e4625e6400d3b
Chris Birchall <chris.birchall@guardian.co.uk>
2015-12-29 13:57:54 +0000

    Get rid of snapshot dependency

--- project/Build.scala
+++ project/Build.scala
@@ -22,7 +22,6 @@ object FirstResponderBuild extends Build {
       scalaVersion := "2.11.7",
       scalacOptions ++= Seq("-feature", "-deprecation"),
       resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases",
-      resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
         libraryDependencies ++= Seq(
         ws,
         "com.amazonaws" % "aws-java-sdk-dynamodb" % Versions.awsSdk,
@@ -30,8 +29,8 @@ object FirstResponderBuild extends Build {
         "com.beachape" %% "enumeratum-play" % "1.3.1",
         "com.gu" %% "play-googleauth" % "0.3.1",
         "com.twilio.sdk" % "twilio-java-sdk" % "4.5.0",
-        "org.scalatestplus" %% "play" % "1.1.1" % "test",
-        "com.adrianhurt" %% "play-bootstrap" % "1.0-P24-B3-SNAPSHOT"
+        "com.adrianhurt" %% "play-bootstrap3" % "0.4.5-P24",
+        "org.scalatestplus" %% "play" % "1.1.1" % "test"
       ),
       riffRaffPackageName := "content-api-first-responder",
       riffRaffManifestProjectName := "Content Platforms::first-responder",
